cada397326a70d557ec4b7a5177353915e741ea6
T_cat9000_2 <terpstragraham@gmail.com>
2024-10-22 19:27:44 -0700

    Create ArithmeticWithVariables.lua

---
+++ src/day2/ArithmeticWithVariables.lua
@@ -0,0 +1,16 @@
+-- Initial values, don't change these!
+player1Score = 10
+player2Score = 10
+
+-- Write your code below:
+print("Kamala scores 1 point!")
+player1Score = player1Score + 1
+print("Kamala score:")
+print(player1Score)
+print("Foul! Bruno loses 2 points!")
+player2Score = player2Score - 2
+print("Bruno score:")
+print(player2Score)
+print("COMBO! Bruno gets a point multuplier!")
+player2Score = player2Score * 1.5
+print(player2Score)
