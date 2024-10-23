e38a362edd9baa944a51de88138073d96cbbfdde
T_cat9000_2 <terpstragraham@gmail.com>
2024-10-22 19:13:38 -0700

    Create varreassignment.lua

---
+++ src/day2/varreassignment.lua
@@ -0,0 +1,11 @@
+-- values
+highestScore = 25
+
+-- code
+highestScorerName = "Kamala"
+print(highestScorerName)
+highestScorerName = "Bruno"
+highestScore = 26
+print("we have a winner!")
+print(highestScorerName)
+print(highestScore)
